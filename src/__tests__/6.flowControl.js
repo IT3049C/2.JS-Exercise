9d3eb459cba9c81253708332ac1a1aa16b9cc13f
Ben Leach <benleach@pm.me>
2020-08-22 22:06:27 -0400

    Text update

--- src/__tests__/6.flowControl.js
+++ src/__tests__/6.flowControl.js
@@ -13,9 +13,9 @@ describe(`5.flowControl`, function () {
   it(`returns buzz on multiple of 5: 10`, () => expect(flowControlAnswers.fizzBuzz(10)).toEqual(`buzz`));
   it(`returns buzz on multiple of 5: 20`, () => expect(flowControlAnswers.fizzBuzz(20)).toEqual(`buzz`));
 
-  it(`returns number when a multiple of 3 and 5: 15`, () => expect(flowControlAnswers.fizzBuzz(15)).toEqual(`fizzbuzz`));
-  it(`returns number when a multiple of 3 and 5: 30`, () => expect(flowControlAnswers.fizzBuzz(30)).toEqual(`fizzbuzz`));
-  it(`returns number when a multiple of 3 and 5: 45`, () => expect(flowControlAnswers.fizzBuzz(45)).toEqual(`fizzbuzz`));
+  it(`returns fizzbuzz when a multiple of 3 and 5: 15`, () => expect(flowControlAnswers.fizzBuzz(15)).toEqual(`fizzbuzz`));
+  it(`returns fizzbuzz when a multiple of 3 and 5: 30`, () => expect(flowControlAnswers.fizzBuzz(30)).toEqual(`fizzbuzz`));
+  it(`returns fizzbuzz when a multiple of 3 and 5: 45`, () => expect(flowControlAnswers.fizzBuzz(45)).toEqual(`fizzbuzz`));
 
   it(`returns false if the argument is not a number`, () => expect(flowControlAnswers.fizzBuzz(`foo`)).toEqual(false));
 });
